3d180aeef770a0d7b7382f06efbc56ec27e8f543
HannachiHassen <hannachihassen07@gmail.com>
2023-07-16 12:37:50 -0400

    Update LoginTest.java class in Test package

--- src/test/java/com/Test/LoginTest.java
+++ src/test/java/com/Test/LoginTest.java
@@ -1,9 +1,21 @@
 package com.Test;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.PageObject.LoginPage;
+
 public final class LoginTest extends BaseTest{
 
 	public LoginTest() {
 		
+	}
+	
+	@Test(priority=1, enabled=true)
+	public void verifyLoginPageTitleTest(){
+		String title = page.getInstance(LoginPage.class).getLoginPageTitle();
+		System.out.println(title);
+		Assert.assertEquals(title, "HubSpot LoginXX");
 	}
 
 }
